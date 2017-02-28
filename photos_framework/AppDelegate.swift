05380a70fd4da52365dd74b9447eef2945c6f8be
Tim Beals <tim.beals@gmail.com>
2017-02-28 14:48:55 -0800

    basic setup of collection view with flow layout

--- photos_framework/AppDelegate.swift
+++ photos_framework/AppDelegate.swift
@@ -19,7 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        window?.rootViewController = ViewController()
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        window?.rootViewController = ViewController(collectionViewLayout: flowLayout)
         
         return true
     }
--- photos_framework/ViewController.swift
+++ photos_framework/ViewController.swift
@@ -8,20 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.cyan
+        setupCollectionView()
     
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupCollectionView() {
+        
+        self.collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        self.collectionView?.backgroundColor = UIColor.white
+        
+        if let layout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout? {
+            
+            print("hello")
+        }
     }
+    
+    
+    //MARK: Collection view delegate methods
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
 
-
+    //MARK: FlowLayoutDelegate
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.bounds.width, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
 }
 
