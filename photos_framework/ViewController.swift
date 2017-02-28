6e1a7cf0d1cd323a49d8a34dd3801138ee98185f
Tim Beals <tim.beals@gmail.com>
2017-02-28 14:26:28 -0800

    basic setup of window and root view controller

---
+++ photos_framework/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  photos_framework
+//
+//  Created by Tim Beals on 2017-02-28.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.cyan
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
